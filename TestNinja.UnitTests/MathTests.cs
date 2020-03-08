c5237f8ca5a1b99eb1b8f07fa9f62caf2cae565e
Galante <fabiogalantemans@gmail.com>
2020-03-08 15:35:16 -0300

    Math

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(5, 3);
+
+            Assert.AreEqual(8, result);
+            Assert.That(result, Is.EqualTo(8));
+        }
+    }
+}
--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -11,53 +11,8 @@ namespace TestNinja.UnitTests
     /// </summary>
     [TestClass]
     public class ReservationTests
-    {
-        public ReservationTests()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+    {    
+        
 
         [TestMethod]
         public void CancBeCancelledBy_UserIsAdmin_ReturnsTrue()
@@ -75,7 +30,7 @@ namespace TestNinja.UnitTests
 
 
         [TestMethod]
-        public void CancBeCancelledBy_NoIsAdminOrUser_ReturnsFalse()
+        public void CancBeCancelledBy_AnotherUser_ReturnsFalse()
         {
             //Arrange
             var reservation = new Reservation();
